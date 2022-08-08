b9e135735e36f5e29b8d39481b1aa3c5e987f39d
Matan Shaviro <matanshaviro@Matans-MBP.lan>
2022-08-08 13:25:57 +0300

    Finish design hotel card component

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { PropertyList } from "./PropertyList/PropertyList";
 export { FeaturedProperties } from "./FeaturedProperties/FeaturedProperties";
 export { MailList } from "./MailList/MailList";
 export { Footer } from "./Footer/Footer";
+export { HotelCard } from "./HotelCard/HotelCard";
